85765a2b0cf3dd198e68562c887eb61fff92b55a
Abrar Shariar <shariarabrar@gmail.com>
2018-12-31 12:16:09 +0600

    delete cache

--- cache/compiled/files/aac4423924a00d943d980f15bf1c50d9.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Applications/MAMP/htdocs/grav-skeleton/user/config/security.yaml',
-    'modified' => 1545716063,
-    'data' => [
-        'salt' => 'jfZ1lLWxi0Arnq'
-    ]
-];
